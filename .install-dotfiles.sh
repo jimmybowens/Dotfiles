65027b3fbb357e9b46014def1cb8d29526903648
Jimmy Bowens <jimmybowens@Jimmys-iMac.fios-router.home>
2017-10-25 12:31:41 -0400

    added zsh support

--- .install-dotfiles.sh
+++ .install-dotfiles.sh
@@ -32,7 +32,23 @@ for plist in $PLISTFILES;do
     ln -s "$plist" "$HOME" > /dev/null 2>&1
   fi
 done
-
+if [ -d "$HOME/.oh-my-zsh" ];then
+  rm -rf "$HOME/.oh-my-zsh"
+fi
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
+if [ -f "$HOME/.zshrc" ];then
+  rm -f "$HOME/.zshrc"
+fi
+cat << EOF > "$HOME/.zshrc"
+export ZSH=/Users/$(whoami)/.oh-my-zsh
+ZSH_THEME="darkblood"
+plugins=(git)
+source $ZSH/oh-my-zsh.sh
+
+export LANG=en_US.UTF-8
+export EDITOR='vim'
+export SSH_KEY_PATH="~/.ssh/rsa_id"
+EOF
 exit 0
